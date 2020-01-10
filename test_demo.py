9d1f47c345e6dbc482ee5b8dc01d9ec0a8c6515f
linnSun <402715005@qq.com>
2020-01-10 16:35:04 +0800

    third commit

---
+++ test_demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/10 16:11
+# @Site    : 
+# @File    : test_demo.py
+# annotation：
+import time
+for i  in range(1,100):
+    print(i)
+    time.sleep(1)
+    i+=1
+
+
+
+
+
+
